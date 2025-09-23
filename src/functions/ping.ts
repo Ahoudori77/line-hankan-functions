8a3b6a93b0fbc436572171712f44b9bcc9c7ff82
poponotann <rakurusu77@gmail.com>
2025-09-23 18:21:15 +0900

    Update ping.ts

--- src/functions/ping.ts
+++ src/functions/ping.ts
@@ -1,7 +1,8 @@
 import { app } from "@azure/functions";
 
 app.http("ping", {
+  route: "ping",
   methods: ["GET"],
-  authLevel: "anonymous",
+  authLevel: "Anonymous",
   handler: async () => ({ status: 200, body: "pong" })
 });
